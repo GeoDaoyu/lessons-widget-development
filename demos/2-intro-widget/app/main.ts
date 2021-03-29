de2c2805fdd7082fbad741795d9d1e69902243f2
zhanggy <geodaoyu@foxmail.com>
2021-03-29 17:10:35 +0800

    2-intro-widget

--- demos/2-intro-widget/app/main.ts
+++ demos/2-intro-widget/app/main.ts
@@ -1,7 +1,5 @@
 import EsriMap from "esri/Map";
 import MapView from "esri/views/MapView";
-import Home from "esri/widgets/Home";
-import HomeViewModel from "esri/widgets/Home/HomeViewModel";
 
 const map = new EsriMap({
   basemap: "streets",
@@ -18,28 +16,10 @@ const view = new MapView({
 //  Widget
 //----------------
 
-// put widget here
+// ...
 
 //----------------
 //  Widget ViewModel
 //----------------
 
-const homeViewModel = new HomeViewModel({
-  view: view,
-});
-
-// <div id="home" class="esri-widget--button esri-widget esri-home">
-//   <span class="esri-icon esri-icon-home"></span>
-// </div>
-const node = document.createElement("div");
-node.id = "home";
-node.innerHTML = `<span class="esri-icon esri-icon-home"></span>`;
-node.className = "esri-widget--button esri-widget esri-home";
-document.getElementById("viewDiv").appendChild(node);
-
-const homeWidget = document.getElementById("home");
-homeWidget.addEventListener("click", function () {
-  homeViewModel.go();
-});
-
-view.ui.add(homeWidget, "top-right");
+// ...
